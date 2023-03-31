36a1c624010926f4a1d86e3479c2eaace17ed27c
Antonio Tomassini <110413106+antonioTOM@users.noreply.github.com>
2023-03-31 14:49:49 +0000

    Program already working in some unperfect way, needs to be completed but looking at console it's already understandable

--- app.js
+++ app.js
@@ -7,3 +7,15 @@ const pickRandomHexColor = () => {
   }
   return color;
 }
+
+const hexToRgb = hex => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  const rgb = {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  }
+  return `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`;
+}
+
+export { pickRandomHexColor, hexToRgb };
