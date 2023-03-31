ef32e67bdf92e80a09923fc4e4879e896a7f5668
Antonio Tomassini <110413106+antonioTOM@users.noreply.github.com>
2023-03-31 14:43:27 +0200

    Update app.js
    Created array with hex color possible chars and function that returns a random hex code color

---
+++ app.js
@@ -0,0 +1,9 @@
+const hexColAvailableChars = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+
+const pickRandomHexColor = () => {
+  let color = '#';
+  for (let x = 0; x < 6; x++) {
+    color += hexColAvailableChars[Math.floor(Math.random() * hexColAvailableChars.length)];
+  }
+  return color;
+}
